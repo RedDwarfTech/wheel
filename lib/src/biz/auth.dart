f3efd766e2b791be7d969fd89a77259e1c60f20e
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-05-31 10:26:27 -0400

    [feature] 新增常用的公共代码

---
+++ lib/src/biz/auth.dart
@@ -0,0 +1,146 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:global_configuration/global_configuration.dart';
+import 'package:wheel/src/biz/user/cruise_user.dart';
+import 'package:wheel/src/biz/user/login_type.dart';
+import 'package:wheel/src/config/global_config.dart';
+import 'package:wheel/src/net/rest/http_result.dart';
+import 'package:wheel/src/net/rest/rest_clinet.dart';
+import 'package:wheel/src/util/common_utils.dart';
+import 'package:wheel/src/util/navigation_service.dart';
+
+
+class AuthResult {
+  String message;
+  Result result;
+
+  AuthResult({
+    required this.message,
+    required this.result,
+  });
+}
+
+class Auth {
+  final baseUrl = GlobalConfiguration().get("baseUrl");
+  static RegExp validationRequired = RegExp(r"Validation required");
+
+  static Future<bool> isLoggedIn() async {
+    final storage = new FlutterSecureStorage();
+    String? username = await storage.read(key: "username");
+    if(username == null){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  static Future<CruiseUser> currentUser() async {
+    final storage = new FlutterSecureStorage();
+    String? userName = await storage.read(key: "username");
+    String? registerTime = await storage.read(key: "registerTime");
+    CruiseUser user = new CruiseUser(phone: userName,registerTime:registerTime );
+    return user;
+  }
+
+  static Future<AuthResult> vote({required String itemId}) async {
+    Map body = {
+      "id": itemId
+    };
+    final response = await RestClient.putHttp("/post/article/upvote",body);
+    if (RestClient.respSuccess(response)) {
+      return AuthResult(message: "SMS send success", result: Result.ok);
+    } else {
+      return AuthResult(
+          message: "SMS send failed. Did you mistype your credentials?",
+          result: Result.error);
+    }
+  }
+
+  static Future<bool> logout() async {
+    final storage = new FlutterSecureStorage();
+    await storage.delete(key: "username");
+    await storage.delete(key: "password");
+    await storage.delete(key: "registerTime");
+    return true;
+  }
+
+  static Future<AuthResult> sms({required String phone}) async {
+    Map body = {
+      "phone": phone
+    };
+    final response = await RestClient.postHttp("/post/user/sms",body);
+    if (RestClient.respSuccess(response)) {
+      final storage = new FlutterSecureStorage();
+      await storage.write(key: "phone", value: phone);
+      return AuthResult(message: "SMS send success", result: Result.ok);
+    } else {
+      return AuthResult(
+          message: response.data["msg"],
+          result: Result.error);
+    }
+  }
+
+  static Future<AuthResult> setPwd({required String phone, required String password}) async {
+    Map body = {
+      "phone": phone,
+      "password": password,
+      "goto": 'news',
+    };
+    final response = await RestClient.postHttp("/post/user/set/pwd",body);
+    if (RestClient.respSuccess(response)) {
+      final storage = new FlutterSecureStorage();
+      await storage.write(key: "username", value: phone);
+      await storage.write(key: "password", value: password);
+      return AuthResult(message: "Login success", result: Result.ok);
+    }  else {
+      return AuthResult(
+          message: "Login failed. Did you mistype your credentials?",
+          result: Result.error);
+    }
+  }
+
+  static Future<AuthResult> verifyPhone({required String phone, required String verifyCode}) async {
+    Map body = {
+      "phone": phone,
+      "verifyCode": verifyCode,
+      "goto": 'news',
+    };
+    final response = await RestClient.postHttp("/post/user/verify",body);
+    if (RestClient.respSuccess(response)) {
+      final storage = new FlutterSecureStorage();
+      await storage.write(key: "phone", value: phone);
+      await storage.write(key: "verifyCode", value: verifyCode);
+      return AuthResult(message: "Login success", result: Result.ok);
+    } else {
+      return AuthResult(
+          message: "Login failed. Did you mistype your credentials?",
+          result: Result.error);
+    }
+  }
+
+  static Future<AuthResult> login({required String username, required String password, required LoginType loginType}) async {
+    List<String> deviceInfo = await CommonUtils.getDeviceDetails();
+    Map body = {
+      "phone": username,
+      "password": password,
+      "goto": 'news',
+      "loginType": loginType.statusCode,
+      "deviceId": deviceInfo[2],
+      "deviceType": int.parse(deviceInfo[1]),
+      "app": 1
+    };
+    final response = await RestClient.postHttpNewDio("/post/user/login",body);
+    if (RestClient.respSuccess(response)) {
+      Map result = response.data["result"];
+      String token = result["token"];
+      String registerTime = result["registerTime"];
+      await storage.write(key: "username", value: username);
+      await storage.write(key: "password", value: password);
+      await storage.write(key: "token", value: token);
+      await storage.write(key: "registerTime", value: registerTime);
+      return AuthResult(message: "Login success", result: Result.ok);
+    } else {
+      NavigationService.instance.navigateToReplacement("login");
+      return AuthResult(message: "Login failed", result: Result.error);
+    }
+  }
+}
---
+++ lib/src/net/rest/rest_clinet.dart
@@ -0,0 +1,50 @@
+import 'package:dio/dio.dart';
+import 'package:global_configuration/global_configuration.dart';
+
+import 'app_interceptors.dart';
+
+class RestClient {
+
+  static Dio dioInstance = Dio(BaseOptions(connectTimeout: 10000, receiveTimeout: 30000, baseUrl: GlobalConfiguration().get("baseUrl")))
+      ..interceptors.add(AppInterceptors());
+
+  static Dio createDio() {
+    // pay attention the dio instance should be single
+    // and the interceptor should add for one
+    // should not be added every time, it may cause multiple duplicate interceptor
+    // this may cause a massive flood http request(important)
+    return dioInstance;
+  }
+
+  static Future<Response> postHttp(String path, Object data) async {
+    final url = GlobalConfiguration().get("baseUrl") + path;
+    Dio dio = createDio();
+    Response response = await dio.post(url, data: data);
+    return response;
+  }
+
+  static Future<Response> postHttpNewDio(String path, Object data) async {
+    final url = GlobalConfiguration().get("baseUrl") + path;
+    Dio dio = new Dio();
+    Response response = await dio.post(url, data: data);
+    return response;
+  }
+
+  static Future<Response> putHttp(String path, Object? data) async {
+    final url = GlobalConfiguration().get("baseUrl") + path;
+    Dio dio = createDio();
+    Response response = await dio.put(url, data: data);
+    return response;
+  }
+
+  static Future<Response> getHttp(String path) async {
+    final url = GlobalConfiguration().get("baseUrl") + path;
+    Dio dio = createDio();
+    Response response = await dio.get(url);
+    return response;
+  }
+
+  static bool respSuccess(Response response) {
+    return response.statusCode == 200 && response.data["statusCode"] == "200" && response.data["resultCode"] == "200";
+  }
+}
