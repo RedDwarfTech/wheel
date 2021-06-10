f6645b0b8c61d35987af79743a26a3c3e80609b4
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-06-10 13:59:49 -0400

    [feature] tweak

--- lib/wheel.dart
+++ lib/wheel.dart
@@ -2,3 +2,4 @@ export 'src/net/rest/rest_clinet.dart';
 export 'src/log/app_log_handler.dart';
 export 'src/net/rest/rest_api_error.dart';
 export 'src/util/common_utils.dart';
+export 'src/config/global_config.dart';
