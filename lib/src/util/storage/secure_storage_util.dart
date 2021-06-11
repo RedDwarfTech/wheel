7045722904a38dd460f1eec100857bdee9f76e84
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-06-11 12:44:25 -0400

    [feature] add secure storage util

---
+++ lib/src/util/storage/secure_storage_util.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorageUtil{
+
+  static FlutterSecureStorage _preferences = FlutterSecureStorage();
+
+  static Future<String?> getString (String key, {String defValue = ''}) {
+    return _preferences.read(key:key) ;
+  }
+
+  static Future<void> putString(String key, String value) {
+    return _preferences.write(key:key, value:value);
+  }
+
+}
+
+
--- lib/wheel.dart
+++ lib/wheel.dart
@@ -3,3 +3,4 @@ export 'src/log/app_log_handler.dart';
 export 'src/net/rest/rest_api_error.dart';
 export 'src/util/common_utils.dart';
 export 'src/config/global_config.dart';
+export 'src/util/storage/secure_storage_util.dart';
