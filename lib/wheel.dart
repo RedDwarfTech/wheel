86eed380fa92900e2715360b15e6e5cfaf4755c1
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-07-28 16:50:07 -0400

    [feature] expose rest log

--- lib/wheel.dart
+++ lib/wheel.dart
@@ -4,3 +4,4 @@ export 'src/net/rest/rest_api_error.dart';
 export 'src/util/common_utils.dart';
 export 'src/config/global_config.dart';
 export 'src/util/storage/secure_storage_util.dart';
+export 'src/log/rest_log.dart';
