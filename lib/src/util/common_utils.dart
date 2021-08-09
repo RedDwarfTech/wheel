f03bca9c518606fce8d16104c56e54cb5c3d31b0
jiang qiang <jiangtingqiang@gmail.com>
2021-08-09 09:55:07 +0800

    [feature] add dev pro diff config

--- lib/src/util/common_utils.dart
+++ lib/src/util/common_utils.dart
@@ -1,25 +1,30 @@
+import 'dart:async';
+import 'dart:io';
 
 import 'package:device_info/device_info.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
-import 'package:global_configuration/global_configuration.dart';
-import 'dart:async';
-import 'package:timeago/timeago.dart' as timeago;
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:wheel/src/config/global_config.dart';
-import 'custom_en.dart';
+import 'package:global_configuration/global_configuration.dart';
 import 'package:in_app_purchase_android/in_app_purchase_android.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import 'package:wheel/src/config/global_config.dart';
 
+import 'custom_en.dart';
 import 'history.dart';
 
-class CommonUtils{
+class CommonUtils {
+  static void initialApp(ConfigType configType) {
+    if (configType == ConfigType.PRO) {
+      GlobalConfiguration().loadFromAsset("pro_app_settings");
+    }
+    if (configType == ConfigType.DEV) {
+      GlobalConfiguration().loadFromAsset("dev_app_settings");
+    }
 
-  static void initialApp() {
-    GlobalConfiguration().loadFromAsset("app_settings");
-    GlobalConfig.init(ConfigType.PRO);
+    GlobalConfig.init(configType);
     WidgetsFlutterBinding.ensureInitialized();
     // Initialize Firebase, collection app crash report
     // https://firebase.flutter.dev/docs/crashlytics/usage/
@@ -44,13 +49,13 @@ class CommonUtils{
         var build = await deviceInfoPlugin.androidInfo;
         deviceName = build.model;
         deviceVersion = build.version.toString();
-        identifier = build.androidId;  //UUID for Android
+        identifier = build.androidId; //UUID for Android
         deviceType = "2";
       } else if (Platform.isIOS) {
         var data = await deviceInfoPlugin.iosInfo;
         deviceName = data.name;
         deviceVersion = data.systemVersion;
-        identifier = data.identifierForVendor;  //UUID for iOS
+        identifier = data.identifierForVendor; //UUID for iOS
         deviceType = "1";
       }
     } on PlatformException {
@@ -59,5 +64,3 @@ class CommonUtils{
     return [deviceName, deviceType, identifier];
   }
 }
-
-
