44d007c2cf09f1bdf0e8771bcce53f4715b5d88d
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-08-14 07:51:25 -0400

    [feature] tweak

--- lib/src/config/global_config.dart
+++ lib/src/config/global_config.dart
@@ -3,9 +3,11 @@ import 'package:global_configuration/global_configuration.dart';
 enum ConfigType { DEV, PRO }
 
 class GlobalConfig {
-  final baseUrl = GlobalConfiguration().get("baseUrl");
-  final shareUrl = GlobalConfiguration().get("shareUrl");
-  final staticResourceUrl = GlobalConfiguration().get("staticUrl");
+  GlobalConfig(this.baseUrl, this.shareUrl, this.staticResourceUrl);
+
+  String baseUrl = GlobalConfiguration().get("baseUrl");
+  String shareUrl = GlobalConfiguration().get("shareUrl");
+  String staticResourceUrl = GlobalConfiguration().get("staticUrl");
 
   static init(ConfigType configType) {
     switch (configType) {
