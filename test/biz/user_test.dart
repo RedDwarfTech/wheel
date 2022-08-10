427dcae7308fbd1ff51ec701f2a452f1de294c53
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-08-10 22:32:11 +0800

    feat: add  user register logic

---
+++ test/biz/user_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:wheel/src/biz/user/user_service.dart';
+import 'package:wheel/wheel.dart';
+
+void main() {
+  test('user register', () {
+    CommonUtils.initialApp(ConfigType.PRO).whenComplete(() => {UserService.regUser(phone: "+15683761628", password: "123")});
+  });
+}
