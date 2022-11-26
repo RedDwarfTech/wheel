566ee19af6dcb8f3802e2cd884bcdd83f00ec996
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-11-27 02:49:08 +0800

    Update auth.dart

--- lib/src/biz/auth.dart
+++ lib/src/biz/auth.dart
@@ -188,7 +188,6 @@ class Auth {
     String accessToken = result["accessToken"];
     String refreshToken = result["refreshToken"];
     String registerTime = result["registerTime"];
-    String userId = result["userId"];
     SecureStorageUtil.putString("username", username);
     SecureStorageUtil.putString("userId", userId);
     SecureStorageUtil.putString("accessToken", accessToken);
